cdc99d6e000aeb7818805c2c16e699cbae025a93
osboo <osboo.osboo@gmail.com>
2021-08-08 13:27:55 +0000

    curl requests for getting bot token from AKB added

---
+++ utils/get_bot_token.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_DIR=$(dirname $(readlink -f "$0"))
+AZURE_AD_TOKEN=$(export $(xargs < $CURRENT_DIR/../.env) && curl -s \
+    --location --request POST "https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/token" \
+    --form "grant_type=client_credentials" \
+    --form "client_id=${APP_ID}" \
+    --form "client_secret=${CLIENT_KEY}" \
+    --form "scope=https://vault.azure.net/.default" | jq -r .access_token)
+
+curl -s --location --request GET 'https://keyvault-teamcityetl.vault.azure.net/secrets/SharegoodToken?api-version=2016-10-01' \
+--header "Authorization: Bearer ${AZURE_AD_TOKEN}" | jq -r .value
--- utils/set-webhook.sh
+++ utils/set-webhook.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CURRENT_DIR=$(dirname $(readlink -f "$0"))
-export $(xargs < $CURRENT_DIR/../.env) && curl -X POST --data-raw \
-    "{\"url\": \"${NGROK_URL}/api/echo_function\"}" -H "Content-Type: application/json" \
+export $(xargs < $CURRENT_DIR/../.env) && \
+    BOT_TOKEN=$(bash $CURRENT_DIR/get_bot_token.sh) && \
+    curl -X POST --data-raw \
+    "{\"url\": \"${NGROK_URL}/api/lebowski\"}" -H "Content-Type: application/json" \
     "https://api.telegram.org/bot${BOT_TOKEN}/setWebhook"
