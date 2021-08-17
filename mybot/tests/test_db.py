589ea2d8abfc15dc08bfa438ded2dc06f62bfbfa
Valentin Pan <valentin_pan@epam.com>
2021-08-17 17:31:27 +0300

    #2 prepare docker-compose

--- mybot/tests/test_db.py
+++ mybot/tests/test_db.py
@@ -1,12 +1,18 @@
 import pytest
+import logging
 from azure.storage.table import TableService
+from lebowski.azure_connections import AKVConnector
 from lebowski.db import DBHelper
 from lebowski.enums import CCY, Tables
 
 
 @pytest.fixture()
 def empty_tables():
-    storage_account = TableService(is_emulated=True)
+    akv = AKVConnector("Not used", "Not used", "Not used", env="dev")
+    storage_account = TableService(connection_string=akv.get_storage_connection_string())
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.info("Connection String " + akv.get_storage_connection_string())
     for table_name in [Tables.SPENDINGS, Tables.MILEAGE, Tables.REMINDERS]:
         storage_account.create_table(table_name)
 
